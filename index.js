9353df874f89ab8708b899bd0eb6e52de8470ca3
Rahul Lasod <rahullasod1234@gmail.com>
2024-05-28 22:33:17 +0530

    intial

---
+++ index.js
@@ -0,0 +1,25 @@
+// var a = 3;
+// var b = 2;
+// var result =a+b;
+// console.log(result)
+
+// if else and else if concept
+
+let age = 12;
+if(age > 18){
+    console.log("go ahead and make vote")
+}
+
+else if(age == 18) {
+         console.log("please go ahead")
+}
+
+else {
+    console.log("you are not elegible to vote")
+}
+
+// ternary operator
+// (condition ? true : false)
+
+let a = 10;
+a === 10 ? console.log("a is equales to 10") : console.log("a is not equales to 10") 
---
+++ index2.js
@@ -0,0 +1,6 @@
+let i = 1;
+while(i <= 10){
+     console.log("14 * ", i, " = ", 14*i);
+     
+     i++;
+}
